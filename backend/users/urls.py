41093d00ea1b588c202f644a180351d12e216658
thecodehen <6582598+thecodehen@users.noreply.github.com>
2020-10-04 18:45:08 +0800

    add signup url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from . import views
+
+app_name = 'users'
 urlpatterns = [
     path('signup/', views.signup , name='signup'),
     path('login/', views.login  , name='login'),
