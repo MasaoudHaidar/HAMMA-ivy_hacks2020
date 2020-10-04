8708e868fac456c96dc5b78476aea7824b102d3d
thecodehen <6582598+thecodehen@users.noreply.github.com>
2020-10-04 10:51:39 +0800

    connect model to problem page

--- backend/course/urls.py
+++ backend/course/urls.py
@@ -6,8 +6,8 @@ app_name = 'course'
 urlpatterns = [
     # e.g., /courses
     path('', views.index, name='index'),
-    # e.g., /courses/1/how-to/
-    path('<int:course_id>/<slug:slug>', views.detail, name='detail'),
+    # e.g., /courses/1
+    path('<int:course_id>', views.detail, name='detail'),
     # e.g., /courses/add_problem/
     path('add_problem/', views.add_problem, name='add_problem'),
     path('process_add_problem/', views.process_add_problem, name='process_add_problem')
