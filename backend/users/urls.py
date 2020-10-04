faa12f404f06dc9e9efaac9ca53e014f67b77536
Mark Jabbour <markmarjabbour@gmail.com>
2020-10-04 00:51:22 -0400

    general sign-up and login pages

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from . import views
 urlpatterns = [
-    path('signup/',  , name='signup'),
-    path('signup/professor',  , name='signup_professor'),
-    path('signup/company',  , name='signup_company'),
-    path('signup/student',  , name='signup_student'),
-    path('login/',  , name='login'),
-    path('login/professor',  , name='login_professor'),
-    path('login/company',  , name='login_company'),
-    path('login/student',  , name='login_student'),
+    path('signup/', views.signup , name='signup'),
+#    path('signup/professor',  , name='signup_professor'),
+#    path('signup/company',  , name='signup_company'),
+#    path('signup/student',  , name='signup_student'),
+    path('login/', views.login  , name='login'),
+#    path('login/professor',  , name='login_professor'),
+#    path('login/company',  , name='login_company'),
+ #   path('login/student',  , name='login_student'),
 ]
