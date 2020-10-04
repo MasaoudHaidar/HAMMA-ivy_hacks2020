2e9c3c654073e0dcc32430e18dd0be721ad1d889
thecodehen <6582598+thecodehen@users.noreply.github.com>
2020-10-04 12:57:53 +0800

    change contact page to about page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/', include('contact.urls')),
+    path('about/', include('contact.urls')),
     path('', include('course.urls')),
 ]
