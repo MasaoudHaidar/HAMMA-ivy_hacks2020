6432bc3e3f093bd5b7561159ef818f7cfafc5b0f
thecodehen <6582598+thecodehen@users.noreply.github.com>
2020-10-04 19:45:45 +0800

    add new user to the corresponding type when registering

--- backend/users/views.py
+++ backend/users/views.py
@@ -10,13 +10,27 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
+            fullname = form.cleaned_data.get('full_name')
             # get role from form and create corresponding account
             role = form.cleaned_data.get('role')
             if role == UserRegisterForm.STUDENT:
-                student_user = StudentUser(
+                student_user = Student(
                     user=User.objects.get(username=username),
+                    full_name=fullname,
                 )
                 student_user.save()
+            elif role == UserRegisterForm.COMPANY:
+                company_user = Company(
+                    user=User.objects.get(username=username),
+                    full_name=fullname,
+                )
+                company_user.save()
+            elif role == UserRegisterForm.PROFESSOR:
+                professor_user = Professor(
+                    user=User.objects.get(username=username),
+                    full_name=fullname,
+                )
+                professor_user.save()
             messages.success(request, f'Account created for {username}!')
             return redirect('users:login')
     else:
